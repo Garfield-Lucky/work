8f59046482127f685fd16ef6355a245e21026975
Garfield-lucky <837072283@qq.com>
2018-08-19 21:45:29 +0800

    首页相关js

---
+++ src/main/webapp/js/index/index.js
@@ -0,0 +1,49 @@
+ $(function(){
+	 
+	 $(".nav").find('.login').click(function(){
+	 	  
+		 window.location.href="login.jsp";
+ 	});
+	 $(".nav").find('.logout').click(function(){
+	 	  
+		 window.location.href="/loginOut";
+ 	});
+
+	
+    //用户管理 使用PageHelper展示
+    $(".mainbody").find('.userManager').click(function(){
+
+        // window.location.href="/loginOut";
+        $('.mainbody_right').load("user/userList");
+ 	});
+
+     //用户管理 使用handlebars展示
+     $(".mainbody").find('.userManager2').click(function(){
+
+         // window.location.href="/loginOut";
+         $('.mainbody_right').load("/user/userManager");
+     });
+
+    
+ });
+
+ function sessionCheck(path){
+	 $.ajax({
+			type : "POST",
+			url : "filter/sessionCheck.do",
+			async:false,
+			data : {
+			},
+			dataType : 'json',
+			success : function(data) {
+				if (data.status == "0000") {
+					$('.mainbody_right').load(path);
+				}else{
+					window.location.href="login.jsp";
+				}
+			},
+			error : function() {
+				alert("error");
+			}
+		});
+ }
