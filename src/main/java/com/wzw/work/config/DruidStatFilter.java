f89be8ae7c6cb21864e63a631de4b67a0842c737
Garfield-lucky <837072283@qq.com>
2018-09-02 00:31:41 +0800

    注释druid注解的方式实现

--- src/main/java/com/wzw/work/config/DruidStatFilter.java
+++ src/main/java/com/wzw/work/config/DruidStatFilter.java
@@ -1,19 +1,19 @@
-package com.wzw.work.config;
-
-import com.alibaba.druid.support.http.WebStatFilter;
-
-import javax.servlet.annotation.WebFilter;
-import javax.servlet.annotation.WebInitParam;
-
-/**
- * @Description: 拦截druid监控请求
- *
- * @param
- * @author Created by wuzhangwei on 2018/8/5 18:05
- */
-@WebFilter(filterName="druidWebStatFilter",urlPatterns="/*",
-        initParams={
-                @WebInitParam(name="exclusions",value="*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*")// 忽略资源
-        })
-public class DruidStatFilter extends WebStatFilter {
-}
+//package com.wzw.work.config;
+//
+//import com.alibaba.druid.support.http.WebStatFilter;
+//
+//import javax.servlet.annotation.WebFilter;
+//import javax.servlet.annotation.WebInitParam;
+//
+///**
+// * @Description: 拦截druid监控请求
+// *
+// * @param
+// * @author Created by wuzhangwei on 2018/8/5 18:05
+// */
+//@WebFilter(filterName="druidWebStatFilter",urlPatterns="/*",
+//        initParams={
+//                @WebInitParam(name="exclusions",value="*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*")// 忽略资源
+//        })
+//public class DruidStatFilter extends WebStatFilter {
+//}
