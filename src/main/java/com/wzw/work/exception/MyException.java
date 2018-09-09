285c82a6a083796dd5cbe8131b03f2cf36a475c9
Garfield-lucky <837072283@qq.com>
2018-09-09 22:31:36 +0800

    添加自定义异常

---
+++ src/main/java/com/wzw/work/exception/MyException.java
@@ -0,0 +1,16 @@
+package com.wzw.work.exception;
+
+import lombok.Data;
+
+@Data
+public class MyException extends RuntimeException {
+
+    public MyException(String code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    private String code;
+    private String msg;
+
+}
