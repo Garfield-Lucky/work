4fb458d7053b0720482e01c4fcc733812730ea67
Garfield-Lucky <wzw971268>
2018-12-09 20:32:05 +0800

    添加文件分片上传

--- src/main/java/com/wzw/work/controller/FileController.java
+++ src/main/java/com/wzw/work/controller/FileController.java
@@ -1,14 +1,19 @@
 package com.wzw.work.controller;
 
+import ch.qos.logback.core.util.ContextUtil;
 import com.alibaba.fastjson.JSONObject;
+import com.wzw.work.consts.WorkConsts;
 import com.wzw.work.entity.GeneralUploadFile;
 import com.wzw.work.entity.User;
 import com.wzw.work.service.GeneralUploadFileService;
-import com.wzw.work.service.KityMindService;
 import com.wzw.work.util.DeleteFileUtil;
+import com.wzw.work.util.ftp.FtpCfgFactory;
+import com.wzw.work.util.ftp.FtpConnectObject;
+import com.wzw.work.util.ftp.FtpUtil;
 import com.wzw.work.util.json.JosnToStrUtil;
 import com.wzw.work.util.page.PageQueryResult;
 import com.wzw.work.util.page.PageSettings;
+import com.wzw.work.vo.ResultVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -60,7 +65,7 @@ public class FileController extends BaseController {
         ModelAndView mav = new ModelAndView("layuiupload");
         return mav;
     }
-
+ 
 
     /**
      * @Description: plupload插件上传
@@ -256,6 +261,72 @@ public class FileController extends BaseController {
             json.put("msg","删除文件失败");
             return json.toJSONString();
         }
+    }
+
+    /**
+     * @Description:
+     * @param file 分片文件
+     * @param fileName 文件名
+     * @param finish 是否分片结束 0：未结束，1：结束
+     * @param chunk 当前是第几个分片
+     * @param timestamp 时间戳
+     * @return
+     * @return ResultVO<Object>
+     * @author Created by wuzhangwei on 2018年11月22日
+     */
+    @RequestMapping(value="/uploadBySlices")
+    public ResultVO<Object> uimCardImportBatchToFtp(@RequestParam("file") MultipartFile file,
+                                                    @RequestParam("fileName") String fileName,
+                                                    @RequestParam("finish") String finish,
+                                                    @RequestParam("chunk") String chunk,
+                                                    @RequestParam("timestamp") String timestamp,
+                                                    @RequestParam("fileNum") String fileNum,
+                                                    @RequestParam("all") String all) {
+
+        ResultVO<Object> result = new ResultVO<>();
+        System.out.println("开始上传");
+        try {
+            String tempFilePath = WorkConsts.SIM_DIR_TEMP;//文件上传的临时目录
+            String ftpPath = WorkConsts.SIM_FTPDIR_PREFIX+"/"+timestamp+"/";//根据时间戳来划分不同时间点上传的文件
+            String file_path = tempFilePath + fileName;//文件存储路径
+            File tmpFile = new File(file_path);
+            RandomAccessFile accessTmpFile = new RandomAccessFile(tmpFile, "rw");
+            long offset = WorkConsts.SIM_CHUNK_SIZE * Long.parseLong(chunk);
+            //定位到该分片的偏移量
+            accessTmpFile.seek(offset);
+            //写入该分片数据
+            accessTmpFile.write(file.getBytes());
+            // 释放
+            accessTmpFile.close();
+//            if(Integer.parseInt(finish) == WorkConsts.SIM_CHUNK_FINISH)//单个文件上传结束
+//            {
+//                //连接ftp服务器
+//                FtpConnectObject obj = new FtpConnectObject();
+//                obj.setFtpWorkDir(ftpPath);
+//                FtpUtil ftp =  FtpCfgFactory.getMyFtp(obj);
+//                //上传文件
+//                ftp.uploadFile(tmpFile, fileName);
+//                //删除临时文件
+//                //tmpFile.delete();
+//
+//
+//            }
+            System.out.println("上传成功");
+
+            result.setCode("0");
+            result.setMsg("上传成功");
+        } catch (IOException e) {
+            result.setCode("1");
+            result.setMsg("文件上传失败!");
+            e.printStackTrace();
+        }catch (Exception e) {
+            result.setCode("1");
+            result.setMsg("文件上传失败!");
+            e.printStackTrace();
+        }
+
+        return result;
+
     }
 
 }
